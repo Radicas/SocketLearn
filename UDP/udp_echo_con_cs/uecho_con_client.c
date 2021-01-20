aed04a46bab852f692392989d20b7e7f3feb7fc2
Radicas <Z1176814811@gmail.com>
2021-01-20 17:15:13 +0800

    UDP协议中加入connect函数，可以用read() write() 读写，

--- UDP/udp_echo_con_cs/uecho_con_client.c
+++ UDP/udp_echo_con_cs/uecho_con_client.c
@@ -13,9 +13,9 @@ int main(int argc, char* argv[])
 {
 	int sock;
 	char message[BUF_SIZE];
+	char message_back[BUF_SIZE];
 	int str_len;
-	socklen_t adr_sz;
-	struct sockaddr_in serv_adr, from_adr;
+	struct sockaddr_in serv_adr;
 	if(argc!=3)
 	{
 		printf("Usage: %s <IP> <port> \n", argv[0]);
@@ -31,6 +31,7 @@ int main(int argc, char* argv[])
 	serv_adr.sin_addr.s_addr = inet_addr(argv[1]);
 	serv_adr.sin_port = htons(atoi(argv[2]));
 	
+	connect(sock, (struct sockaddr*)&serv_adr, sizeof(serv_adr));
 	while(1)
 	{
 		fputs("Insert message(q to quit): ", stdout);
@@ -38,12 +39,13 @@ int main(int argc, char* argv[])
 		if(!strcmp(message, "q\n") || !strcmp(message, "Q\n"))
 			break;
 
-		sendto(sock, message, strlen(message), 0, (struct sockaddr*)&serv_adr, sizeof(serv_adr));
+		write(sock, message, strlen(message));
+		read(sock, message_back, strlen(message));
+//		sendto(sock, message, strlen(message), 0, (struct sockaddr*)&serv_adr, sizeof(serv_adr));
 
-		adr_sz = sizeof(from_adr);
-		str_len = recvfrom(sock, message, BUF_SIZE, 0, (struct sockaddr*)&from_adr, &adr_sz);
-		message[str_len] = 0;
-		printf("Message from server: %s", message);
+//		adr_sz = sizeof(from_adr);
+//		str_len = recvfrom(sock, message_back, BUF_SIZE, 0, (struct sockaddr*)&from_adr, &adr_sz);
+		printf("Message from server: %s", message_back);
 
 	}
 	close(sock);
