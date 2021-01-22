55c68ce129be57a4eb42396d3e22b4b372f68528
Radicas <Z1176814811@gmail.com>
2021-01-22 21:57:20 +0800

    fork函数复制进程

---
+++ fork.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <unistd.h>
+
+int gval = 10;
+int main(int argc, char* argv[])
+{
+	pid_t pid;
+	int lval = 20;
+	gval++;
+	lval+=5;
+	
+	pid = fork();
+	if(pid==0)
+		gval += 2, lval +=2;
+	else
+		gval -=2, gval -=2;
+
+	if(pid==0)
+		printf("Child process: [%d, %d] \n", gval, lval);
+	else
+		printf("Father process: [%d, %d] \n", gval, lval);
+	return 0;
+}
